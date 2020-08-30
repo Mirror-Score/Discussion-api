3b7dc01315bf5c38630715d10094d3451b219529
Naman Tamrakar <namantam1@gmail.com>
2020-08-30 12:27:58 +0530

    changed database

--- myproject/settings.py
+++ myproject/settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'khqmovke',
-#         'USER': 'khqmovke',
-#         'PASSWORD': 'hcjyoUU3vnCqy88lR1No-vLzs7XwnsBF',
-#         'HOST': 'lallah.db.elephantsql.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'khqmovke',
+        'USER': 'khqmovke',
+        'PASSWORD': 'hcjyoUU3vnCqy88lR1No-vLzs7XwnsBF',
+        'HOST': 'lallah.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
